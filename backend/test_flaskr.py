4d8af76e6e2abfed2d6ae9ec17e8066f73c4c304
Gary Vladimir <gary@garybricks.com>
2024-06-07 17:24:54 -0600

    added 1 test for each operation

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -13,15 +13,20 @@ class TriviaTestCase(unittest.TestCase):
     def setUp(self):
         """Define test variables and initialize app."""
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
-        
-        self.app = create_app({
-            "SQLALCHEMY_DATABASE_URI": self.database_path
-        })
+        self.database_path = (
+            f"postgresql://postgres:genio123@localhost:5432/{self.database_name}"
+        )
+
+        self.app = create_app({"SQLALCHEMY_DATABASE_URI": self.database_path})
 
         self.client = self.app.test_client
+        setup_db(self.app, self.database_path)
 
-    
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            self.db.create_all()
+
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -30,6 +35,76 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+
+    def test_get_categories(self):
+        res = self.client().get("/categories")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["categories"])
+
+    def test_get_paginated_questions(self):
+        res = self.client().get("/questions?page=1")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["questions"])
+        self.assertTrue(data["total_questions"])
+        self.assertTrue(data["categories"])
+
+    def test_delete_question(self):
+        question = Question(
+            question="New question", answer="New answer", difficulty=1, category=1
+        )
+        question.insert()
+        question_id = question.id
+
+        res = self.client().delete(f"/questions/{question_id}")
+        data = json.loads(res.data)
+
+        question = Question.query.filter(Question.id == question_id).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertEqual(data["deleted"], question_id)
+        self.assertEqual(question, None)
+
+    def test_post_new_question(self):
+        new_question = {
+            "question": "New question",
+            "answer": "New answer",
+            "difficulty": 1,
+            "category": 1,
+        }
+        res = self.client().post("/questions", json=new_question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+
+    def test_search_questions(self):
+        res = self.client().post("/questions", json={"searchTerm": "question"})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["questions"])
+        self.assertTrue(data["total_questions"])
+        self.assertTrue(data["current_category"])
+
+    def test_quiz(self):
+        quiz_data = {
+            "previous_questions": [],
+            "quiz_category": {"type": "Science", "id": "1"},
+        }
+        res = self.client().post("/quizzes", json=quiz_data)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["question"])
 
 
 # Make the tests conveniently executable
