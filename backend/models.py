91828ff3a42ad8b0a45a03667ee8de5ddff842d2
Gary Vladimir <gary@garybricks.com>
2024-06-10 16:10:15 -0600

    replaced hardcoded local database info with env variables info

--- backend/models.py
+++ backend/models.py
@@ -3,8 +3,12 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "trivia"
-database_path = f"postgresql://postgres:genio123@localhost:5432/{database_name}"
+database_name = os.getenv("DATABASE_NAME")
+database_user = os.getenv("DATABASE_USER")
+database_password = os.getenv("DATABASE_PASSWORD")
+database_host = os.getenv("DATABASE_HOST")
+database_port = os.getenv("DATABASE_PORT")
+database_path = f"postgresql://{database_user}:{database_password}@{database_host}:{database_port}/{database_name}"
 
 db = SQLAlchemy()
 
